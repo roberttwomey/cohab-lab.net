fb3c17f336103ba4578030c7809bb83d2721c650
Robert Twomey <robert@roberttwomey.com>
2024-10-25 05:58:33 -0700

    added rss feed consumer

---
+++ _plugins/rss-feed.rb
@@ -0,0 +1,22 @@
+require 'open-uri'
+require 'nokogiri'
+
+module Jekyll
+  class RssFeed < Generator
+    safe true
+
+    def generate(site)
+      feed_url = "https://roberttwomey.com/rss" # Replace with your RSS feed URL
+      doc = Nokogiri::XML(URI.open(feed_url))
+
+      site.data['rss_feed_items'] = doc.xpath('//item').map do |item|
+        {
+          title: item.at_xpath('title').content,
+          link: item.at_xpath('link').content,
+          description: item.at_xpath('description').content,
+          pubDate: item.at_xpath('pubDate').content
+        }
+      end
+    end
+  end
+end
